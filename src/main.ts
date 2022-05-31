9d10e62c6e51bf0a4503946328fc1ac97d970587
xingcdev <60323726+xingcdev@users.noreply.github.com>
2022-05-31 15:15:20 +0200

    add css import of boostrap

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App.vue';
+import 'bootstrap/dist/css/boostrap.css';
+import 'bootstrap-vue/dist/boostrap-vue.css';
 
 Vue.use(BootstrapVue);
-
 Vue.config.productionTip = false;
 
 new Vue({
